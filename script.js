cb05d98d86b218b9231a4ccd8a1baf9baa3be739
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-08-24 14:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	let arr = Object.entries(sampleObject);
-	for(let i=0 ; i<arr.length ; i++){
-		if(arr[i][0] === key) return true;
-	}
-	return false;
+	if(sampleObject[key] != undefined) return true;
+    else return false; 
 }
 
 // Do not change the code below
